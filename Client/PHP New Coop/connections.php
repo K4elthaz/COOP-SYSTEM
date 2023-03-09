0841b76dc1eb7725e901b9c8731943ecceb5c529
Jeremy Daniel <jalcantarabedtc@gmail.com>
2023-03-09 08:35:09 +0800

    connections
    initialize

---
+++ Client/PHP New Coop/connections.php
@@ -0,0 +1,7 @@
+<?php
+$connections = mysqli_connect("localhost", "root", "", "coop-database");
+
+if (mysqli_connect_errno()) {
+    echo "failed to connect to MYSQL: " . mysqli_connect_error();
+}
+?>
