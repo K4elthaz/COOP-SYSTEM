cc168836b10366a08964d913567b6939bffed911
Satsuu <lumawagjamesbryan@gmail.com>
2023-03-09 08:38:03 +0800

    Add Edit Description

---
+++ Client/EditProfile.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="../assets//css//bootstrap.min.css"/>
+    <link rel="stylesheet" href="./ERDBClientSideEditProfile.css" />
+
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,700&display=swap"
+    />
+
+    <link rel="stylesheet" href="./EditProfile.css">
+  </head>
+  <body>
+  <div class="container-xl px-4 mt-4">
+    <!-- Account page navigation-->
+    <nav class="nav nav-borders">
+        <a class="nav-link active ms-0" href="https://www.bootdey.com/snippets/view/bs5-edit-profile-account-details" target="__blank">Profile</a>
+    </nav>
+    <hr class="mt-0 mb-4">
+    <div class="row">
+        <div class="col-xl-4">
+            <!-- Profile picture card-->
+            <div class="card mb-4 mb-xl-0">
+                <div class="card-header">Profile Picture</div>
+                <div class="card-body text-center">
+                    <!-- Profile picture image-->
+                    <img class="img-account-profile rounded-circle mb-2" src="../Client/IDPicture.jpg" alt="">
+                    <!-- Profile picture help block-->
+                    <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+                    <!-- Profile picture upload button-->
+                    <button class="btn btn-primary" type="button">Upload new image</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-8">
+            <!-- Account details card-->
+            <div class="card mb-4">
+                <div class="card-header">Account Details</div>
+                <div class="card-body">
+                    <form>
+                        <div class="row gx-3 mb-3">
+                            <!-- Form Group (surname)-->
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputFirstName">Surname</label>
+                                <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your surname" value="">
+                            </div>
+                            <!-- Form Group (given name)-->
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputLastName">Given name</label>
+                                <input class="form-control" id="inputLastName" type="text" placeholder="Enter your given name" value="">
+                            </div>
+                        </div>
+                        <!-- Form Row-->
+                        <div class="row gx-3 mb-3">
+                            <!-- Form Group (middle name)-->
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputFirstName">Middle name</label>
+                                <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your middle name" value="">
+                            </div>
+                            <!-- Form Group (last name)-->
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputLastName">Extension name</label>
+                                <input class="form-control" id="inputLastName" type="text" placeholder="Enter your extension name" value="">
+                            </div>
+                        </div>
+                        <!-- Form Row        -->
+                        <div class="row gx-3 mb-3">
+                            <!-- Form Group (organization name)-->
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputOrgName">Email Address</label>
+                                <input class="form-control" id="inputOrgName" type="text" placeholder="Enter your organization name" value="">
+                            </div>
+                            <!-- Form Group (location)-->
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputLocation">Contact No.</label>
+                                <input class="form-control" id="inputLocation" type="text" placeholder="Enter your location" value="09xxxxxxxxx">
+                            </div>
+                        </div>
+                        <!-- Form Group (email address)-->
+                        <div class="mb-3">
+                            <label class="small mb-1" for="inputEmailAddress">Address</label>
+                            <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter your current address" value="">
+                        </div>
+                        <!-- Form Row-->
+                        <div class="row gx-3 mb-3">
+                            <!-- Form Group (phone number)-->
+                            <div class="col-md-6">
+                                <label class="small mb-1" for="inputPhone">TIN</label>
+                                <input class="form-control" id="inputPhone" type="tel" placeholder="Enter your TIN number" value="">
+                            </div>
+                            <!-- Form Group (gender)-->
+                            <div class="col-md-2">
+                                <label class="small mb-1" for="inputBirthday">Gender</label>
+                                <input class="form-control" id="inputBirthday" type="text" name="birthday" placeholder="Gender" value="Male/Female">
+                            </div>
+                            <!-- Form Group (birthday)-->
+                            <div class="col-md-2">
+                                <label class="small mb-1" for="inputBirthday">Birthday</label>
+                                <input class="form-control" id="inputBirthday" type="text" name="birthday" placeholder="Enter your birthday" value="06/10/1988">
+                            </div>
+                        </div>
+                        <!-- Save changes button-->
+                        <button class="btn btn-primary" type="button">Save changes</button>
+                        <script src="../assets//js//bootstrap.min.js"></script>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  </body>
+</html>
