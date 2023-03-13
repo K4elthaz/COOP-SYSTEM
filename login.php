bdbd2b8ddab069ad11d6739ef47d3576163cdae4
Jeremy Daniel <jalcantarabedtc@gmail.com>
2023-03-13 15:39:52 +0800

    Update login.php

--- login.php
+++ login.php
@@ -84,7 +84,7 @@ if (isset($_POST["btnLogin"])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Login V1</title>
+<title>Login ERDB-Multipurpose Cooperative</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -94,9 +94,9 @@ if (isset($_POST["btnLogin"])) {
 <link rel="stylesheet" type="text/css" href="assets/vendor/animate/animate.css">
 <link rel="stylesheet" type="text/css" href="assets/vendor/css-hamburgers/hamburgers.min.css">
 <link rel="stylesheet" type="text/css" href="assets/vendor/select2/select2.min.css">
-<link rel="stylesheet" type="text/css" href="assets/css/util.css">
-<link rel="stylesheet" type="text/css" href="assets/css/main.css">
-<link rel="stylesheet" href="main.css">
+<link rel="stylesheet" type="text/css" href="util.css">
+<link rel="stylesheet" type="text/css" href="main.css">
+<link rel="stylesheet" type="text/css" href="main.css">
 
 <meta name="robots" content="noindex, follow">
 <script nonce="a822a476-1b98-4997-af61-6d989d0e9924">(function(w,d){!function(bv,bw,bx,by){bv[bx]=bv[bx]||{};bv[bx].executed=[];bv.zaraz={deferred:[],listeners:[]};bv.zaraz.q=[];bv.zaraz._f=function(bz){return function(){var bA=Array.prototype.slice.call(arguments);bv.zaraz.q.push({m:bz,a:bA})}};for(const bB of["track","set","debug"])bv.zaraz[bB]=bv.zaraz._f(bB);bv.zaraz.init=()=>{var bC=bw.getElementsByTagName(by)[0],bD=bw.createElement(by),bE=bw.getElementsByTagName("title")[0];bE&&(bv[bx].t=bw.getElementsByTagName("title")[0].text);bv[bx].x=Math.random();bv[bx].w=bv.screen.width;bv[bx].h=bv.screen.height;bv[bx].j=bv.innerHeight;bv[bx].e=bv.innerWidth;bv[bx].l=bv.location.href;bv[bx].r=bw.referrer;bv[bx].k=bv.screen.colorDepth;bv[bx].n=bw.characterSet;bv[bx].o=(new Date).getTimezoneOffset();if(bv.dataLayer)for(const bI of Object.entries(Object.entries(dataLayer).reduce(((bJ,bK)=>({...bJ[1],...bK[1]})))))zaraz.set(bI[0],bI[1],{scope:"page"});bv[bx].q=[];for(;bv.zaraz.q.length;){const bL=bv.zaraz.q.shift();bv[bx].q.push(bL)}bD.defer=!0;for(const bM of[localStorage,sessionStorage])Object.keys(bM||{}).filter((bO=>bO.startsWith("_zaraz_"))).forEach((bN=>{try{bv[bx]["z_"+bN.slice(7)]=JSON.parse(bM.getItem(bN))}catch{bv[bx]["z_"+bN.slice(7)]=bM.getItem(bN)}}));bD.referrerPolicy="origin";bD.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(bv[bx])));bC.parentNode.insertBefore(bD,bC)};["complete","interactive"].includes(bw.readyState)?zaraz.init():bv.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document);</script></head>
@@ -113,8 +113,11 @@ if (isset($_POST["btnLogin"])) {
 
 <form class="login100-form validate-form" method="POST">
 <span class="login100-form-title">
-ERDB Multi-Purpose Cooperative
+ERDB <br>Multi-Purpose Cooperative
 </span>
+
+
+
 <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
 
 <input class="input100" type="text" name="email" placeholder="Username" value ="<?php echo $email;?>">
