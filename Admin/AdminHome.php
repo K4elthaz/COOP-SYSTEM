356570f0d51f426392743c0b5a0e819272f3ee5b
yellyAC <breva_3190584@letran-calamba.edu.ph>
2023-03-15 09:18:00 +0800

    changed the table

--- Admin/AdminHome.php
+++ Admin/AdminHome.php
@@ -6,13 +6,13 @@
 
     <link rel="stylesheet" href="../assets//css//bootstrap.min.css"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,700&display=swap"
     />
 
-    <link rel="stylesheet" href="./AdminHome.css">
+    <link rel="stylesheet" href="AdminHome.css">
   </head>
   <body>
   <div class="container-xl px-4 mt-4">
@@ -34,107 +34,149 @@
             </div>
         </div>
 
-        <div class="col-xl-8">
+        <div class="col-xl-8" style>
             <div class="card mb-4" id="form">
-                <nav class="card-header" id="btn">
-                    <!-- <a class="card-header active ms-0" >YOUR BALANCE</a> -->
-                    <h4> YOUR BALANCE </h4>
-                </nav>
-                <div class="card-body">
-                <td>
-					<a class="add" title="Add" data-toggle="tooltip"><i class="material-icons"></i></a>
-                    <a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons"></i></a>
-                    <a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons"></i></a>
-                </td>
-                <table class="table table-success table-striped">
-                    <thead>
-                        <tr style="text-align: center;">
-                        <th scope="col">Header</th>
-                        <th scope="col">Balance</th>
-                        <th scope="col">Date</th>
-                        <th scope="col">Loan Amount</th>
-                        <th scope="col">Term</th>
-                        <th scope="col">Monthly</th>
-                        <th scope="col">Months Default</th>
-                        <th scope="col">Arrears</th>
-                        </tr>
-                    </thead>
-                    <tbody style="text-align: center;">
-                        <tr>
-                            <th scope="row">EMERGENCY</th>
-                            <td>150,000.00</td>
-                            <td>03/01/2023</td>
-                            <td>200,000.00</td>
-                            <td>2</td>
-                            <td>1,000.00</td>
-                            <td>0.68</td>
-                            <td>1,234.00</td>
-                        </tr>
+                    <nav class="card-header" id="btn">
+                        <!-- <a class="card-header active ms-0" >YOUR BALANCE</a> -->
+                        <h4> ADMIN </h4>     
+                    </nav>
+                    <div class="card-body">
+                        <form class="example" action="action_page.php">
+                            <a class="add" title="Add" data-toggle="tooltip"><i class="material-icons"></i></a>
+                            <a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons"></i></a>
+                            <a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons"></i></a>
+                            <input type="text" placeholder="Search.." name="search">
+                            <button type="submit"><i class="fa fa-search"></i></button>   
+                        </form>
+                 
+                        <br>
                         
-                        <tr>
-                            <th scope="row">PETTY CASH</th>
-                            <td>150,000.00</td>
-                            <td>03/01/2023</td>
-                            <td>200,000.00</td>
-                            <td>4</td>
-                            <td>1,000.00</td>
-                            <td>0.68</td>
-                            <td>1,234.00</td>
-                        </tr>
+                        <!-- table -->
+                        <div class="table-container">
+                            <table>
+                                <thead>
+                                <tr style="text-align: center;">
+                                    <th>Header</th>
+                                    <th>Balance</th>
+                                    <th>Date</th>
+                                    <th>Loan Amount</th>
+                                    <th>Term</th>
+                                    <th>Monthly</th>
+                                    <th>Months Default</th>
+                                    <th>Arrears</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>Row 1, Column 1</td>
+                                    <td>Row 1, Column 2</td>
+                                    <td>Row 1, Column 3</td>
+                                    <td>Row 1, Column 4</td>
+                                    <td>Row 1, Column 5</td>
+                                    <td>Row 1, Column 6</td>
+                                    <td>Row 1, Column 7</td>
+                                    <td>Row 1, Column 8</td>
 
-                        <tr>
-                            <th scope="row">STL</th>
-                            <td>150,000.00</td>
-                            <td>03/01/2023</td>
-                            <td>200,000.00</td>
-                            <td>4</td>
-                            <td>1,000.00</td>
-                            <td>0.68</td>
-                            <td>1,234.00</td>
-                        </tr>
+                                </tr>
+                                <tr>
+                                    <td>Row 2, Column 1</td>
+                                    <td>Row 2, Column 2</td>
+                                    <td>Row 2, Column 3</td>
+                                    <td>Row 2, Column 4</td>
+                                    <td>Row 2, Column 5</td>
+                                    <td>Row 2, Column 6</td>
+                                    <td>Row 2, Column 7</td>
+                                    <td>Row 2, Column 8</td>
+                                </tr>
 
-                        <tr>
-                            <th scope="row">SAVINGS REPORT</th>
-                            <td>150,000.00</td>
-                            <td>03/01/2023</td>
-                            <td>200,000.00</td>
-                            <td>2</td>
-                            <td>1,000.00</td>
-                            <td>0.68</td>
-                            <td>1,234.00</td>
-                        </tr>
+                                <tr>
+                                    <td>Row 2, Column 1</td>
+                                    <td>Row 2, Column 2</td>
+                                    <td>Row 2, Column 3</td>
+                                    <td>Row 2, Column 4</td>
+                                    <td>Row 2, Column 5</td>
+                                    <td>Row 2, Column 6</td>
+                                    <td>Row 2, Column 7</td>
+                                    <td>Row 2, Column 8</td>
+                                </tr>
 
-                        <tr>
-                            <th scope="row">SHARE</th>
-                            <td>150,000.00</td>
-                            <td>03/01/2023</td>
-                            <td>200,000.00</td>
-                            <td>3</td>
-                            <td>1,000.00</td>
-                            <td>0.68</td>
-                            <td>1,234.00</td>
-                        </tr>
+                                <tr>
+                                    <td>Row 2, Column 1</td>
+                                    <td>Row 2, Column 2</td>
+                                    <td>Row 2, Column 3</td>
+                                    <td>Row 2, Column 4</td>
+                                    <td>Row 2, Column 5</td>
+                                    <td>Row 2, Column 6</td>
+                                    <td>Row 2, Column 7</td>
+                                    <td>Row 2, Column 8</td>
+                                </tr>
 
-                        <tr>
-                            <th scope="row">SPECIAL-PROMO</th>
-                            <td>150,000.00</td>
-                            <td>03/01/2023</td>
-                            <td>200,000.00</td>
-                            <td>6</td>
-                            <td>1,000.00</td>
-                            <td>0.68</td>
-                            <td>1,234.00</td>
-                        </tr>
+                                <tr>
+                                    <td>Row 2, Column 1</td>
+                                    <td>Row 2, Column 2</td>
+                                    <td>Row 2, Column 3</td>
+                                    <td>Row 2, Column 4</td>
+                                    <td>Row 2, Column 5</td>
+                                    <td>Row 2, Column 6</td>
+                                    <td>Row 2, Column 7</td>
+                                    <td>Row 2, Column 8</td>
+                                </tr>
+
+                                <tr>
+                                    <td>Row 2, Column 1</td>
+                                    <td>Row 2, Column 2</td>
+                                    <td>Row 2, Column 3</td>
+                                    <td>Row 2, Column 4</td>
+                                    <td>Row 2, Column 5</td>
+                                    <td>Row 2, Column 6</td>
+                                    <td>Row 2, Column 7</td>
+                                    <td>Row 2, Column 8</td>
+                                </tr>
+
+                                <tr>
+                                    <td>Row 2, Column 1</td>
+                                    <td>Row 2, Column 2</td>
+                                    <td>Row 2, Column 3</td>
+                                    <td>Row 2, Column 4</td>
+                                    <td>Row 2, Column 5</td>
+                                    <td>Row 2, Column 6</td>
+                                    <td>Row 2, Column 7</td>
+                                    <td>Row 2, Column 8</td>
+                                </tr>
+
+                                <tr>
+                                    <td>Row 2, Column 1</td>
+                                    <td>Row 2, Column 2</td>
+                                    <td>Row 2, Column 3</td>
+                                    <td>Row 2, Column 4</td>
+                                    <td>Row 2, Column 5</td>
+                                    <td>Row 2, Column 6</td>
+                                    <td>Row 2, Column 7</td>
+                                    <td>Row 2, Column 8</td>
+                                </tr>
+
+                                <tr>
+                                    <td>Row 2, Column 1</td>
+                                    <td>Row 2, Column 2</td>
+                                    <td>Row 2, Column 3</td>
+                                    <td>Row 2, Column 4</td>
+                                    <td>Row 2, Column 5</td>
+                                    <td>Row 2, Column 6</td>
+                                    <td>Row 2, Column 7</td>
+                                    <td>Row 2, Column 8</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                            </div>
 
 
-                    </tbody>
-                    </table>
 
+                        <!-- table -->
+
+
+                    </div>
                 </div>
             </div>
-
-            
-
         </div>
     </div>
 </div>
@@ -142,3 +184,6 @@
 </body>
 </html>
 
+<?php
+include("../connections.php");
+?>
--- connections.php
+++ connections.php
@@ -2,6 +2,76 @@
 $connections = mysqli_connect("localhost", "root", "", "coop-database");
 
 if (mysqli_connect_errno()) {
-    echo "failed to connect to MYSQL: " . mysqli_connect_error();
+  echo "failed to connect to MYSQL: " . mysqli_connect_error();
 }
 ?>
+
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+/* ADMIN - Style the search field */
+form.example input[type=text] {
+  padding: 1px;
+  font-size: 17px;
+  border: 1px solid grey;
+  border-bottom-left-radius: 7px;
+  border-top-left-radius: 7px;
+  float: left;
+  width: 30%;
+  background: #f1f1f1;
+  margin-left: 0%;
+  margin-top: 0%
+}
+
+/* ADMIN - Style the submit button */
+form.example button {
+  float: left;
+  width: 5%;
+  padding: 1px;
+  background: #2196F3;
+  color: white;
+  font-size: 17px;
+  border: 1px solid grey;
+  border-bottom-right-radius: 7px;
+  border-top-right-radius: 7px;
+  border-left: none; /* Prevent double borders */
+  cursor: pointer;
+  margin-right: 54%;
+}
+
+form.example button:hover {
+  background: #0b7dda;
+}
+
+/* Clear floats */
+form.example::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+/* tables */
+.table-container {
+  width: 100%;
+  overflow-x: auto;
+  overflow-y: auto;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th, td {
+  padding: 8px;
+  text-align: center;
+  border: 1px solid blue;
+  color: black;
+
+}
+
+
+</style>
