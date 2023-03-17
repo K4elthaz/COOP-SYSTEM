a5f49f3f50b52e0c42fa7b55e1c895f73becc552
Jeremy Daniel <jalcantarabedtc@gmail.com>
2023-03-18 02:32:00 +0800

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,11 +1,17 @@
 <?php
+
 require __DIR__."/vendor/autoload.php";
 
 use Kreait\Firebase\Factory;
+use Kreait\Firebase\Auth;
+
+
+
 
 $factory = (new Factory)
     ->withServiceAccount('erdb-multi-purpose-coope-7a7d9-firebase-adminsdk-ba6v0-df775153e2.json')
     ->withDatabaseUri('https://erdb-multi-purpose-coope-7a7d9-default-rtdb.asia-southeast1.firebasedatabase.app/');
 
 $database = $factory->createDatabase();
+$auth = $factory->createAuth();
 ?>
