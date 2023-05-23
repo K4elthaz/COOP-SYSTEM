8d69ad26f6e90f6120f2dc6a1a6cc9d7db3213cc
yellyAC <breva_3190584@letran-calamba.edu.ph>
2023-05-23 14:44:42 +0800

    improved download forms

--- Admin/pdfUpload.php
+++ Admin/pdfUpload.php
@@ -28,7 +28,7 @@
                             <a class="nav-link active" aria-current="page" href="controlPanel.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="../login.php">Logout</a>
+                            <a class="nav-link active" aria-current="page" href="../logout.php">Logout</a>
                         </li>
                     </ul>
                 </div>
